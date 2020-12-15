e59227e25530851e9024046b54fcb1b26a354380
ahmad-elkhawaldeh <60712751+ahmad-elkhawaldeh@users.noreply.github.com>
2020-12-15 12:57:46 -0500

    Update loop2.py

--- loop2.py
+++ loop2.py
@@ -1,12 +1,14 @@
+
 #!/usr/bin/env python3
 
 # Created by: Ahmad El-khawaldeh
 # Created on: Dec 2020
-# This program uses a while loop
+# This program asks the user for a positive integer,
+# then multiplies all the whole numbers up to that number 
 
 
 def main():
-
+ 
     # input
     positive_integer = print("  Enter how many times to repeat ")
     positive_string = input("Enter Here plz : ")
@@ -16,11 +18,14 @@ def main():
     # process & output
     try:
         positive_integer = int(positive_string)
-
-        while loop_counter < positive_integer:
-            loop_counter = loop_counter + 1
-            number_factorial = number_factorial * loop_counter
-        print("The factorial of {0} is {1}".format(num, number_factorial))
+        
+        if positive_integer < 0:
+            print(" it's not a valid number ")
+        else:
+            while loop_counter < positive_integer:
+                loop_counter = loop_counter + 1
+                number_factorial = number_factorial * loop_counter
+            print("The factorial of {0} is {1}".format(positive_integer, number_factorial))
 
     except Exception:
         print("This was an invalid number ")
@@ -28,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
